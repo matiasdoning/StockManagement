045ee332ee085e1fa943fd44f9cc78c1f07b3b9a
matiasdoning <71906855+matiasdoning@users.noreply.github.com>
2026-02-09 22:41:35 +0100

    Actualizar Mail stock 1h.py

--- Mail stock 1h.py
+++ Mail stock 1h.py
@@ -97,9 +97,9 @@ def generate_signals(df, open_positions):
 
 # 6️⃣ Enviar email
 def send_email(df_buy, df_sell, open_positions):
-    sender_email = os.environ.get("EMAIL_USER")
+    sender_email = os.environ.get("matiasdoning@gmail.com")
     sender_password = os.environ.get("rgzh scts sxhn ilkp")
-    recipient_email = os.environ.get("EMAIL_USER")
+    recipient_email = os.environ.get("matiasdoning@gmail.com")
 
     subject = f"📊 Trading Hourly Alert - {datetime.now().strftime('%d/%m/%Y %H:%M')}"
     body = f"<h2>📈 Señales de Trading - {datetime.now().strftime('%d/%m/%Y %H:%M')}</h2>"
